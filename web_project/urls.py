488db43e931b894c22fa3c05477c2505f79c8f88
AKO PRECIOUS <akoprecious58@gmail.com>
2023-10-20 19:02:45 -0400

    adding routes

--- web_project/urls.py
+++ web_project/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view
-from products.views import product_detail_view
+from products.views import product_detail_view, product_create_view, product_update_view, product_delete_view 
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('product/', product_detail_view, name='home'),
+    path('create/', product_create_view, name='product-list'),
+    path('<int:id>/', product_detail_view, name='product-detail'),
+    path('<int:id>/update/', product_update_view, name='product-update'),
+    path('<int:id>/delete/', product_delete_view, name='product-delete'),
     path('admin/', admin.site.urls),
 ]
