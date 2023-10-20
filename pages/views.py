56a6d6581b2b26ca0a544d3b8ff58b1709e2e325
AKO PRECIOUS <akoprecious58@gmail.com>
2023-10-20 18:44:27 -0400

    pproduct details

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,11 @@
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# def home_view(request, *args,**kwargs):
+#     return HttpResponse("<h1> Hello world </h1>")
+
+
+def home_view(request, *args,**kwargs):
+    return render(request, "home.html", {})
--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 # def home_view(request, *args,**kwargs):
 #     return HttpResponse("<h1> Hello world </h1>")
 
 
-def home_view(request, *args,**kwargs):
-    return render(request, "home.html", {})
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    context = {
+        'object': obj,
+        
+    }
+    return render(request, "product/detail.html", context)
